5761e7a0562a8a725e2d74ffca0c521879e75ce1
agradess <ajgradess@gmail.com>
2022-02-24 19:52:05 -0500

    at least halfway done with LifecycleAware, error w/ ViewModelProvider

--- Labs/Lab4_LifecycleAware/app/src/main/java/course/labs/lab5_lifecycle_aware/CounterViewModel.kt
+++ Labs/Lab4_LifecycleAware/app/src/main/java/course/labs/lab5_lifecycle_aware/CounterViewModel.kt
@@ -1,9 +1,37 @@
 package course.labs.lab5_lifecycle_aware
+
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-//ToDo: define your own view model that would be used to store the data when activity is destroyed during rotation
+//TODO: define your own view model that would be used to store the data when activity is destroyed during rotation
 
-class CounterViewModel : ViewModel() {
+class CounterViewModel : ViewModel(), LifecycleObserver {
+
+    // storing two values, orientation and counter
+    // refer to slides and example (LifecycleAwareTicker)
+
+    companion object {
+        private const val TAG = "TickerViewModel"
+    }
+
+    private val orientation = MutableLiveData<String>()
+    private val counter = MutableLiveData<Int>()
+
+    // Access methods for the activity, best practice to pass non-mutable data
+    internal val iOrientation: LiveData<String>
+        get() = orientation
+    internal val iCounter: LiveData<Int>
+        get() = counter
+
+    init {
+        orientation.value = "Portrait"
+        counter.value = 0
+    }
+
+    internal fun bindToActivityLifecycle(lifecycleMainActivity: LifecycleMainActivity) {
+        lifecycleMainActivity.lifecycle.addObserver(this)
+    }
 
 }
--- Labs/Lab4_LifecycleAware/app/src/main/java/course/labs/lab5_lifecycle_aware/LifecycleMainActivity.kt
+++ Labs/Lab4_LifecycleAware/app/src/main/java/course/labs/lab5_lifecycle_aware/LifecycleMainActivity.kt
@@ -7,26 +7,31 @@ import android.view.View
 import android.widget.TextView
 import android.content.Context
 import android.view.WindowManager
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 
 class LifecycleMainActivity : AppCompatActivity() {
+
+    private lateinit var model : CounterViewModel
+
     private fun getScreenOrientation(context: Context): String {
         val orientationList = listOf("Portrait","Landscape","Reverse Portrait","Reverse Landscape")
         val orientation = (context.getSystemService(Context.WINDOW_SERVICE) as WindowManager).defaultDisplay.rotation
         return orientationList[orientation]
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //ToDo: Implement your own logic to display appropriate text ,increase and reset the counter
 
         //Todo: Initialize reset button, display text, and 'NEXT' button
-        val resetButton: Button = null as Button
-        val prButton:Button = null as Button
-        val displayText: TextView = null as TextView
+        val resetButton: Button = findViewById<Button>(R.id.ResButton) as Button
+        val prButton:Button = findViewById<Button>(R.id.PrButton) as Button
+        val displayText: TextView = findViewById<TextView>(R.id.DisplayText) as TextView
 
-        //Todo: Initialize mode for counter using CounterViewModel
-        // var model = ...
+        // Todo: Initialize model for counter using CounterViewModel
+         model = ViewModelProvider(this).get(CounterViewModel::class.java)
 
         prButton.setOnClickListener {
             //Todo: Increment counter and update display text
@@ -34,7 +39,7 @@ class LifecycleMainActivity : AppCompatActivity() {
         }
 
         resetButton.setOnClickListener {
-            //Todo: Reset counter and updte display text
+            //Todo: Reset counter and update display text
         }
     }
 
