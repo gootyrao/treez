c6248ee74f80385283dba1983c1f3bcbacc2defc
Andrej Rasevic <andrej@rasevicengineering.com>
2022-03-01 09:16:26 -0500

    adding UI Lab

---
+++ Labs/Lab5_UILabs/app/src/main/java/course/labs/todomanager/ToDoListAdapter.kt
@@ -0,0 +1,123 @@
+package course.labs.todomanager
+
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.core.app.ActivityCompat.startActivityForResult
+import androidx.recyclerview.widget.RecyclerView
+import course.labs.todomanager.ToDoItem.Status
+
+class ToDoListAdapter(private val mContext: Context) :
+    RecyclerView.Adapter<ToDoListAdapter.ViewHolder>() {
+
+    private val mItems = ArrayList<ToDoItem>()
+
+    // Add a ToDoItem to the adapter
+    // Notify observers that the data set has changed
+
+    fun add(item: ToDoItem) {
+        mItems.add(item)
+        notifyItemChanged(mItems.size)
+    }
+
+    // Clears the list adapter of all items.
+    fun clear() {
+        mItems.clear()
+        notifyDataSetChanged()
+    }
+
+    fun getItem(pos: Int): Any {
+        return mItems[pos - 1]
+    }
+
+    // Returns the number of ToDoItems
+
+    override fun getItemCount(): Int {
+        return mItems.size + 1
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (position==0) HEADER_VIEW_TYPE else TODO_VIEW_TYPE
+    }
+
+    // Retrieve the number of ToDoItems
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        if (viewType == HEADER_VIEW_TYPE) {
+            val v = LayoutInflater.from(parent.context).inflate(R.layout.header_view, parent, false)
+            return ViewHolder(v)
+        } else {
+            val v = LayoutInflater.from(parent.context).inflate(R.layout.todo_item, parent, false)
+            val viewHolder = ViewHolder(v)
+
+            // TODO - Inflate the View (defined in todo_item.xml) for this ToDoItem and store references in ViewHolder
+
+
+            return viewHolder
+        }
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+
+        if (position == 0) {
+            viewHolder.itemView.setOnClickListener {
+                Log.i(ToDoManagerActivity.TAG, "Entered footerView.OnClickListener.onClick()")
+                val options: Bundle? = null
+                startActivityForResult(
+                    mContext as Activity,
+                    Intent(
+                        mContext,
+                        AddToDoActivity::class.java
+                    ),
+                    ToDoManagerActivity.ADD_TODO_ITEM_REQUEST,
+                    options
+                )
+            }
+        } else {
+            val toDoItem = mItems[position - 1]
+
+            Log.i(TAG, "onBindViewHolder   " + viewHolder.mTitleView.toString())
+
+            viewHolder.mTitleView!!.text = toDoItem.title
+
+            // Todo - Initialize statusView's isChecked property
+
+            // TODO - Set up OnCheckedChangeListener CheckBox
+
+            // TODO - Display Priority in a TextView
+
+
+            // TODO - Display Time and Date
+
+        }
+    }
+
+    // Get the ID for the ToDoItem
+    // In this case it's just the position
+
+    override fun getItemId(pos: Int): Long {
+        return pos.toLong() - 1
+    }
+
+    class ViewHolder internal constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var mItemLayout: View = itemView
+        var mTitleView: TextView? = null
+        var mStatusView: CheckBox? = null
+        var mPriorityView: TextView? = null
+        var mDateView: TextView? = null
+    }
+
+    companion object {
+        private const val TAG = "Lab-UserInterface"
+        private const val HEADER_VIEW_TYPE = R.layout.header_view
+        private const val TODO_VIEW_TYPE = R.layout.todo_item
+    }
+
+
+}
---
+++ Labs/Lab5_UILabs/app/src/main/java/course/labs/todomanager/ToDoManagerActivity.kt
@@ -0,0 +1,171 @@
+package course.labs.todomanager
+
+import android.app.Activity
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.FileNotFoundException
+import java.io.IOException
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.io.PrintWriter
+import java.text.ParseException
+import java.util.Date
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import course.labs.todomanager.ToDoItem.Priority
+import course.labs.todomanager.ToDoItem.Status
+
+class ToDoManagerActivity : Activity() {
+
+    private lateinit var mAdapter: ToDoListAdapter
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.recycle_view)
+        Log.i(TAG, "Entered onCreate()")
+
+        // Todo - Create a new TodoListAdapter for this Activity's RecyclerView
+
+        // Load saved ToDoItems
+        loadItemsFromFile()
+
+        // TODO - Attach the adapter to this Activity's RecyclerView
+
+
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        Log.i(TAG, "Entered onActivityResult()")
+
+        // TODO - Check result code and request code
+        // if user submitted a new ToDoItem
+        // Create a new ToDoItem from the data Intent
+        // and then add it to the adapter
+
+    }
+
+    // Do not modify below here
+
+    public override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        // Save ToDoItems to file
+        saveItemsToFile()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu)
+
+        menu.add(Menu.NONE, MENU_DELETE, Menu.NONE, "Delete all")
+        menu.add(Menu.NONE, MENU_DUMP, Menu.NONE, "Dump to log")
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            MENU_DELETE -> {
+                mAdapter.clear()
+                true
+            }
+            MENU_DUMP -> {
+                dump()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun dump() {
+        for (i in 1 until mAdapter.itemCount) {
+            val data = (mAdapter.getItem(i) as ToDoItem).toLog()
+            Log.i(TAG,
+                    "Item " + i + ": " + data.replace(ToDoItem.ITEM_SEP!!, ","))
+        }
+    }
+
+    // Load stored ToDoItems
+    private fun loadItemsFromFile() {
+        var reader: BufferedReader? = null
+        try {
+            val fis = openFileInput(FILE_NAME)
+            reader = BufferedReader(InputStreamReader(fis))
+
+            var title: String?
+            var priority: String?
+            var status: String?
+            var date: Date?
+
+            do {
+                title = reader.readLine()
+                if (title == null)
+                    break
+                priority = reader.readLine()
+                status = reader.readLine()
+                date = ToDoItem.FORMAT.parse(reader.readLine())
+                mAdapter.add(ToDoItem(title, Priority.valueOf(priority),
+                        Status.valueOf(status), date))
+
+            }
+            while (true)
+
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } catch (e: ParseException) {
+            e.printStackTrace()
+        } finally {
+            if (null != reader) {
+                try {
+                    reader.close()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+
+            }
+        }
+    }
+
+    // Save ToDoItems to file
+    private fun saveItemsToFile() {
+        var writer: PrintWriter? = null
+        try {
+            val fos = openFileOutput(FILE_NAME, Context.MODE_PRIVATE)
+            writer = PrintWriter(BufferedWriter(OutputStreamWriter(
+                    fos)))
+
+            for (idx in 1 until mAdapter.itemCount) {
+
+                writer.println(mAdapter.getItem(idx))
+
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } finally {
+            writer?.close()
+        }
+    }
+
+    companion object {
+
+        const val ADD_TODO_ITEM_REQUEST = 0
+        private const val FILE_NAME = "TodoManagerActivityData.txt"
+        const val TAG = "Lab-UserInterface"
+
+        // IDs for menu items
+        private const val MENU_DELETE = Menu.FIRST
+        private const val MENU_DUMP = Menu.FIRST + 1
+    }
+}
